5cfb1cbaa172fc32138dfee4391955fa39adcf34
Ivan Bodnarash <bodnarash1999@gmail.com>
2025-12-07 20:55:22 +0100

    feat: working on Navbar with styles, layout, setting google font

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,45 +1,27 @@
 "use client";
 
+import Image from "next/image";
 import { useFavorites } from "@/context/FavoritesContext";
-import { useRouter, useSearchParams } from "next/navigation";
 import styles from "./Navbar.module.css";
+import marvelLogo from "../../../public/marvel_logo.png";
+import favIcon from "../../../public/heart_icon.png";
+import Link from "next/link";
 
 export function Navbar() {
   const { favoritesCount } = useFavorites();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const favoritesActive = searchParams.get("favorites") === "1";
-
-  const handleLogoClick = () => {
-    router.push("/");
-  };
-
-  const handleFavoritesClick = () => {
-    // toggle ?favorites=1 in URL
-    if (favoritesActive) {
-      router.push("/");
-    } else {
-      router.push("/?favorites=1");
-    }
-  };
 
   return (
     <header className={styles.navbar}>
       {/* Logo */}
-      <button className={styles.logo} onClick={handleLogoClick} title="Go to character list">
-        {/* Logo from Figma */}
-        MARVEL
-      </button>
+      <Link href="/" title="Go to character list">
+        <Image src={marvelLogo} alt="logo" className={styles.logo} />
+      </Link>
 
       {/* Favorites */}
       <div className={styles.favoritesBlock}>
-        <button
-          className={`${styles.favoritesButton}${favoritesActive ? styles.active : ""}`}
-          onClick={handleFavoritesClick}
-        >
-          ♥
-        </button>
+        <Link href="/?favorites" className={styles.favoritesButton}>
+          <Image src={favIcon} alt="fav_icon" className={styles.favIcon} />
+        </Link>
         <span className={styles.badge}>{favoritesCount}</span>
       </div>
     </header>
